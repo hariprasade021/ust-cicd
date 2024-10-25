3e85446a1b8704829d0372588a907714032980f4
Hariprasad E <Hariprasad.E@ust.com>
2024-10-25 10:49:29 +0530

    3rd commit

--- src/main/java/com/ust/cicd/api/MyApi.java
+++ src/main/java/com/ust/cicd/api/MyApi.java
@@ -1,9 +1,12 @@
 package com.ust.cicd.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyApi {
+    @GetMapping
     public String Hello(){
         return "Hello World";
     }
